3638dfd35052342159b3c20b0b3b0366a29c64ce
Mai <trinhphuongmai13@gmail.com>
2022-08-26 23:38:12 +0700

    Added the search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Testing</div>;
+  let [word, setWord] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Looking for ${word}`);
+  }
+
+  function updateWord(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a word to look up"
+          onChange={updateWord}
+        />
+        <button type="submit" value="search">
+          search
+        </button>
+      </form>
+    </div>
+  );
 }
